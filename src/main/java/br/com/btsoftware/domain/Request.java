8f77164ab33e3ccea1897f332615357d122c4432
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-08-30 11:25:45 -0300

    feito mapeamento dos domains

--- src/main/java/br/com/btsoftware/domain/Request.java
+++ src/main/java/br/com/btsoftware/domain/Request.java
@@ -1,11 +1,13 @@
 package br.com.btsoftware.domain;
 
 
+import br.com.btsoftware.domain.enums.RequestState;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -13,12 +15,30 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name = "requests")
 public class Request {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 75, nullable = false)
     private String subject;
+
+    @Column(columnDefinition = "text")
     private String description;
+
+    @Column(name = "create_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
     private Date createDate;
-    private RequestStage state;
+
+    @Column(length = 12, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private RequestState state;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @OneToMany(mappedBy = "request")
     private List<RequestStage> stages =  new ArrayList<RequestStage>();
 }
--- src/main/java/br/com/btsoftware/domain/User.java
+++ src/main/java/br/com/btsoftware/domain/User.java
@@ -6,19 +6,37 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter @Setter
+@Entity(name="users")
 public class User {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 75, nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
     private Role role;
+
+    @OneToMany(mappedBy = "user")
     private List<Request> requests = new ArrayList<Request>();
+
+    @OneToMany(mappedBy = "user")
     private List<RequestStage> stages = new ArrayList<RequestStage>();
 
 }
