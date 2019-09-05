b7e1da0f45bcb4afbc608ba91f34ebcfef12ceb7
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-09-05 09:18:51 -0300

    Ajuste(colocado a anotation @Service na classe RequestService)

--- src/main/java/br/com/btsoftware/service/RequestService.java
+++ src/main/java/br/com/btsoftware/service/RequestService.java
@@ -4,11 +4,13 @@ import br.com.btsoftware.domain.Request;
 import br.com.btsoftware.domain.enums.RequestState;
 import br.com.btsoftware.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class RequestService {
     @Autowired
     private RequestRepository requestRepository;
