b7a8930f23f39b71ac8d1234767b61c2ff29e77b
Benjamim-Thiago <bebenjamimthiago@gmail.com>
2019-09-12 08:20:17 -0300

    ajustando ResourceException colocango anotation

--- src/main/java/br/com/btsoftware/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/btsoftware/resource/exception/ResourceExceptionHandler.java
@@ -5,11 +5,14 @@ import java.util.Date;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import br.com.btsoftware.exception.NotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(NotFoundException.class)
 	public ResponseEntity<ApiError> handleNotFoundException(NotFoundException ex){
 		ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
 				
